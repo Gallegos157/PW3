008984d035c16793bdf3e179f535f3474d2db354
Gallegos157 <78167445+Gallegos157@users.noreply.github.com>
2021-04-16 01:52:09 -0500

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,43 @@
+
+
+
+
+	
+		function fecha1(){
+             
+             document.getElementById('el-resultado').innerHTML = "Primera computadora";
+             document.getElementById('el-resultado1').innerHTML="";
+            
+             document.getElementById('el-resultado2').innerHTML = '< img src="img/1.jpg" class="card-img-top" alt="..." width="100px" height="300px"/>';
+            }
+
+function fecha2(){
+             
+             document.getElementById('el-resultado').innerHTML = "Primer lenguaje de programación FORTAN ";
+             document.getElementById('el-resultado1').innerHTML="";
+             document.getElementById('el-resultado1').innerHTML = '<img src "img/2.jpg" class="card-img-top" alt="..." width="100px" height="300px"/>';
+
+            }
+
+
+            function fecha3(){
+             
+             document.getElementById('el-resultado').innerHTML = "Perl";
+             document.getElementById('el-resultado1').innerHTML="";
+             document.getElementById('el-resultado1').innerHTML = '<img src "img/3.jpg" class="card-img-top" alt="..." width="100px" height="300px"/>';
+
+            }
+            function fecha4(){
+             
+             document.getElementById('el-resultado').innerHTML = "creación del lenguaje PHP";
+             document.getElementById('el-resultado1').innerHTML="";
+             document.getElementById('el-resultado1').innerHTML = '<img src "img/4.jpg" class="card-img-top" alt="..." width="100px" height="300px"/>';
+
+            }
+            function fecha5 (){
+             
+             document.getElementById('el-resultado').innerHTML = "Creación de Javascricpt";
+             document.getElementById('el-resultado1').innerHTML="";
+             document.getElementById('el-resultado1').innerHTML = '< img src "img/6.png" class="card-img-top" alt="..." width="100px" height="300px"/>';
+
+            }
